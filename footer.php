221d0dd37e3d42e51a7c2bd888b03a618d1311f3
Ashish Aryal <ashisharyal@gmail.com>
2023-06-21 08:29:25 +0545

    initial commit

---
+++ footer.php
@@ -0,0 +1,21 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>contact info</h3>
+         <p> <i class="fas fa-envelope"></i> ashisharyal5000@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> Butwal </p>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="https://www.facebook.com/"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="https://www.instagram.com/"> <i class="fab fa-instagram"></i> instagram </a>
+      </div>
+
+   </div>
+
+   <p class="credit"> &copy; copyright <span>Bookalaya--2023</span> </p>
+
+</section>
